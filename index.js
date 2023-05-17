803ed8aa29ddb3ccb3046314001f381a399adfa5
Executor <just4dotaalex@gmail.com>
2023-05-18 00:31:42 +0300

    feat: set up main file

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const setupDB = require('./src/configs/db');
 require('dotenv').config();
+
+const usersRouter = require('./src/routes/users.routes');
+const booksRouter = require('./src/routes/books.routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use(usersRouter);
+app.use(booksRouter);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server started ${process.env.PORT}`);
-})
+});
+
+setupDB();
