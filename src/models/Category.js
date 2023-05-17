1dbbef4428b92b70d15ac9cefb5d79770830cd1f
Executor <just4dotaalex@gmail.com>
2023-05-17 23:23:11 +0300

    feat: added model for category

---
+++ src/models/Category.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const categorySchema = Schema({
+  _id: { type: String },
+  name: { type: String },
+});
+
+const Category = new model('categories', categorySchema);
+
+module.exports = { Category };
