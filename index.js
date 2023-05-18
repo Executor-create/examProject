1f276ddc61477abe0834938eaa21cdfa0ea68844
Executor <just4dotaalex@gmail.com>
2023-05-18 19:51:19 +0300

    feat: added categories router

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const usersRouter = require('./src/routes/users.routes');
 const booksRouter = require('./src/routes/books.routes');
+const categoriesRouter = require('./src/routes/categories.routes');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.json());
 
 app.use(usersRouter);
 app.use(booksRouter);
+app.use(categoriesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started ${process.env.PORT}`);
