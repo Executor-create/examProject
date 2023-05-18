90284b86e54bdaa13b2c97288d2e6517bfe1f876
Executor <just4dotaalex@gmail.com>
2023-05-19 02:03:59 +0300

    feat: added patch route for category

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -4,5 +4,6 @@ const categoriesControllers = require('../controllers/categories.controllers');
 const router = Router();
 
 router.post('/categories', categoriesControllers.addCategory);
+router.patch('/categories/:_id', categoriesControllers.updateCategory);
 
 module.exports = router;
