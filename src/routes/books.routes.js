1899da67ba37765817856a1a8b2132d55ae05f96
Executor <just4dotaalex@gmail.com>
2023-05-18 19:53:10 +0300

    feat: added books router

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const booksControllers = require('../controllers/books.controllers');
 
 const router = Router();
+
+router.post('/books', booksControllers.addBook);
 
 module.exports = router;
