7c7ba0e3d68640b3426386771a2d6610d3c6d547
Executor <just4dotaalex@gmail.com>
2023-05-18 20:13:56 +0300

    feat: added get router

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -4,5 +4,6 @@ const booksControllers = require('../controllers/books.controllers');
 const router = Router();
 
 router.post('/books', booksControllers.addBook);
+router.get('/books/:_id', booksControllers.getBook);
 
 module.exports = router;
