701433a0ee34a681701d3d45d738ac6370ae87fd
Executor <just4dotaalex@gmail.com>
2023-05-20 00:45:29 +0300

    feat: added router for getBookByFilter

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post('/books', booksControllers.addBook);
 router.get('/books/:_id', booksControllers.getBook);
+router.get('/books', booksControllers.getBookByFilter);
 
 module.exports = router;
