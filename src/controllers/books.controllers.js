2cfe69f4038d56597fa2b58994544bd512f6b229
Executor <just4dotaalex@gmail.com>
2023-05-20 00:45:06 +0300

    feat: added getBookByFilter controller

--- src/controllers/books.controllers.js
+++ src/controllers/books.controllers.js
@@ -40,7 +40,33 @@ const getBook = async (req, res) => {
   res.status(200).send(book);
 }
 
+const getBookByFilter = async (req, res) => {
+  const { categories, title, search, isAvailable } = req.query;
+
+  const filters = {};
+
+  if (categories) {
+    const categoryArray = categories.split(',').map((category) => new RegExp(category, 'i'));
+    filters.category = { $in: categoryArray };
+  }
+
+  if (title)
+    filters.title = { $regex: title, $options: 'i' };
+
+  if (search) {
+    filters.$or = [
+      { title: { $regex: search, $options: 'i' } },
+      { 'description.short': { $regex: search, $options: 'i' } },
+      { 'description.full': { $regex: search, $options: 'i' } },
+    ]
+  };
+
+  const books = await Book.find(filters);
+  res.status(200).send(books);
+}
+
 module.exports = {
   addBook,
   getBook,
+  getBookByFilter,
 }
